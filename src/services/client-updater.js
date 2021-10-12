1e73e411cdd537752b8be948e93e5e0e69879a70
Slava Trofimov <my@pmb.link>
2021-10-11 20:11:39 -0400

    minor download upd

--- src/services/client-updater.js
+++ src/services/client-updater.js
@@ -4,17 +4,24 @@ const fs = require('fs')
 const os = require('os')
 
 exports.updateClient = async () => {
+    //downloading a client archive
     let distroHost = process.env.VUE_APP_DISTRO_HOST
     ipcRenderer.send(
         'clientDownload', {
-        url: distroHost + "/client.zip",
+        url: distroHost + "/client-dummy.zip",
         properties: { directory: os.tmpdir() }
     }
     )
     ipcRenderer.on("download progress", (event, progress) => {
-        console.log('Download Progress: '+progress.percent)
+        //callback for client download
+        let percent = Math.round(progress.percent*100)
+        let total = Math.round(progress.totalBytes / 1000000)
+        console.log('Download Progress: ' + percent)
+        console.log('Total:' + total + ' Mb')
+        console.log(progress)
     })
     ipcRenderer.on("download complete", async (event, filePath) => {
+        //client downloaded to a temp folder
         console.log(filePath)
         console.log('prepairing a dir')
         //removing client folder and recreating it
@@ -23,9 +30,11 @@ exports.updateClient = async () => {
             fs.mkdirSync('./client');
         }
         console.log('done perpairing a dir')
+        //unziping a client to it's folder
         var zip = new admZip(filePath)
         console.log('start unzip')
         zip.extractAllTo("./client", true)
+        //unziping complete, removing temp file
         console.log('finished unzip')
         fs.unlinkSync(filePath, console.log('temp file deleted'))
     })
